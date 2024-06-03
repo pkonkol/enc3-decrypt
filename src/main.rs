88476a6a0f8e58db87b50350e34639bf9a63f457
pkonkol <31186361+pkonkol@users.noreply.github.com>
2024-06-03 11:54:05 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,94 @@
+use std::io::Read;
 
-const DELTA: i32 = 0x9e3779b9;
+use flate2::read::ZlibDecoder;
+
+const DELTA: u32 = 0x9e3779b9;
 
 // now what was that stupid macro?
-fn mx(y: u32, z: u32, sum: u32, key: &[u32], p: u32, e: u32) -> u32 {
-    ((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[((p&3)^e) as usize] ^ z))
+fn mx(y: u32, z: u32, sum: u32, key: &[u32], p: usize, e: usize) -> u32 {
+    ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)) ^ ((sum ^ y) + (key[((p & 3) ^ e) as usize] ^ z))
 }
 
-fn bdecrypt(buffer &[u8], len i64, k u64) {
-    const u32 key: [u32] = [k >> 32, k & 0xFFFFFFFF, 0xDEADDEAD, 0xB00BEEEF]
-    let y, z, sum;
-    let v: [u32] = [1,2,3];
-    let n = (len - len%4) / 4;
+fn bdecrypt(buffer: &mut [u8], len: isize, k: u64) {
+    let key: &[u32] = &[
+        (k >> 32) as u32,
+        (k & 0xFFFFFFFF) as u32,
+        0xDEADDEAD,
+        0xB00BEEEF,
+    ];
+    let (mut y, mut z, mut sum): (u32, u32, u32);
+    // v should be basically the pointer to the buffer
+    // let v: &mut [u32] = &mut [1, 2, 3]; //  uint32_t *v = (uint32_t *) buffer;
+    // what does the type transform from uint8_t to uint32_t and how to do it in rust
+    let v = buffer;
+
+    let (mut p, rounds, mut e): (usize, usize, usize);
+    let n = (len - len % 4) / 4;
     if n < 2 {
         return;
     }
-    rounds = 6 + 52/n;
-    sum = round * DELTA;
-    y = v[0]
+    let mut rounds = (6 + 52 / n) as u32;
 
+    sum = rounds * DELTA;
+    // TODO read and access more than u8 from the buffer
+    y = v[0..4];
+    loop {
+        e = ((sum >> 2) & 3) as usize;
+        p = n as usize - 1;
+        while p > 0 {
+            z = v[n as usize - 1];
+            // y = v[0] -= mx(y, z, sum, key, p, e);
+            v[0] -= mx(y, z, sum, key, p, e);
+            y = v[0];
+            p -= 1;
+        }
+        z = v[n as usize - 1];
+        // this should be how c++ executes it in order
+        // y = (v[0] -= mx(y, z, sum, key, p, e));
+        v[0] -= mx(y, z, sum, key, p, e);
+        y = v[0];
+        sum -= DELTA;
+        if rounds == 0 {
+            break;
+        }
+        rounds -= 1;
+    }
 }
 
-fn decryptBuffer() {
+fn decryptBuffer(buffer: &[u8]) -> bool {
+    if buffer.len() < 5 {
+        return true;
+    }
+    if buffer[0..4] != b"ENC3"[0..4] {
+        // will this comparison work?
+        return false;
+    }
+    // should it be be or le bytes?
+    let key: u64 = u64::from_be_bytes(buffer[4..12].try_into().unwrap());
+    let compressed_size = u32::from_be_bytes(buffer[12..16].try_into().unwrap());
+    let size = u32::from_be_bytes(buffer[16..20].try_into().unwrap());
+    let adler = u32::from_be_bytes(buffer[20..24].try_into().unwrap());
+    if (compressed_size as usize) < buffer.len() - 24 {
+        return false;
+    }
+    bdecrypt(buffer, compressed_size as isize, key);
 
+    let mut z = ZlibDecoder::new(&buffer[24..]);
+    let mut s = String::new();
+    z.read_to_string(&mut s).unwrap();
+    println!(s);
+    // let mut new_buffer = String::new();
+    // new_buffer.reserve(size as usize);
+    return false;
 }
 
-// this should go through all the files in the directory, copy unencrypted and write decrypted 
+// this should go through all the files in the directory, copy unencrypted and write decrypted
 // to the new outputDirectory so that we get perffect unencrypted mirror
-fn createFS() {
-
-}
+fn createFS() {}
 
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,100 @@
+use super::*;
+
+#[test]
+fn test_test() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+}
+
+static input: &'static [u8] = &[];
+
+const output: &str = r##"-- CONFIG
+APP_NAME = "Askara_v4"  -- important, change it, it's name for config dir and files in appdata
+APP_VERSION = 1343       -- client version for updater and login to identify outdated client
+DEFAULT_LAYOUT = "retro" -- on android it's forced to "mobile", check code bellow
+
+-- If you don't use updater or other service, set it to updater = ""
+Services = {
+  website = "https://askara.net", -- currently not used
+  updater = "http://askara.otupdate.ovh/updater.php",
+  stats = "",
+  crash = "http://otclient.ovh/api/crash.php",
+  feedback = "http://otclient.ovh/api/feedback.php",
+  status = "http://askara.otupdate.ovh/status.php"
+}
+
+-- Servers accept http login url, websocket login url or ip:port:version
+Servers = {
+	Kasteria = "0.0.0.0:7191:854:12:22:25:30:78:80:90:91:98:99:101:104:106:114:115:116:120:124:125"
+}
+
+--USE_NEW_ENERGAME = true -- uses entergamev2 based on websockets instead of entergame
+ALLOW_CUSTOM_SERVERS = false -- if true it shows option ANOTHER on server list
+
+g_app.setName("Askara")
+-- CONFIG END
+
+-- print first terminal message
+g_logger.info(os.date("== application started at %b %d %Y %X"))
+g_logger.info(g_app.getName() .. ' ' .. g_app.getVersion() .. ' rev ' .. g_app.getBuildRevision() .. ' (' .. g_app.getBuildCommit() .. ') made by ' .. g_app.getAuthor() .. ' built on ' .. g_app.getBuildDate() .. ' for arch ' .. g_app.getBuildArch())
+
+if not g_resources.directoryExists("/data") then
+  g_logger.fatal("Data dir doesn't exist.")
+end
+
+if not g_resources.directoryExists("/modules") then
+  g_logger.fatal("Modules dir doesn't exist.")
+end
+
+--[[settings
+g_configs.loadSettings("/config.otml")]]
+
+-- set layout
+local settings = g_configs.getSettings()
+local layout = DEFAULT_LAYOUT
+if g_app.isMobile() then
+  layout = "mobile"
+elseif settings:exists('layout') then
+  layout = settings:getValue('layout')
+end
+g_resources.setLayout(layout)
+
+-- load mods
+g_modules.discoverModules()
+g_modules.ensureModuleLoaded("corelib")
+  
+local function loadModules()
+  -- libraries modules 0-99
+  g_modules.autoLoadModules(99)
+  g_modules.ensureModuleLoaded("gamelib")
+
+  -- client modules 100-499
+  g_modules.autoLoadModules(499)
+  g_modules.ensureModuleLoaded("client")
+
+  -- game modules 500-999
+  g_modules.autoLoadModules(999)
+  g_modules.ensureModuleLoaded("game_interface")
+
+  -- mods 1000-9999
+  g_modules.autoLoadModules(9999)
+end
+
+g_proxy.addProxy("88.198.137.48", 7191, 0)
+g_proxy.addProxy("54.37.139.130", 7191, 0)
+g_proxy.addProxy("51.75.161.186", 7191, 0)
+g_proxy.addProxy("51.83.42.200", 7191, 0)
+
+-- report crash
+if type(Services.crash) == 'string' and Services.crash:len() > 4 and g_modules.getModule("crash_reporter") then
+  g_modules.ensureModuleLoaded("crash_reporter")
+end
+
+-- run updater, must use data.zip
+if type(Services.updater) == 'string' and Services.updater:len() > 4 
+  and g_resources.isLoadedFromArchive() and g_modules.getModule("updater") then
+  g_modules.ensureModuleLoaded("updater")
+  return Updater.init(loadModules)
+end
+loadModules()
+"##;
