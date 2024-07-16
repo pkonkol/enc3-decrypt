4984c05d5dbd294dd57ae238ff576aa184d5e04b
pkonkol <31186361+pkonkol@users.noreply.github.com>
2024-07-16 10:20:46 +0200

    complete but failing algorithm using Wrapping<u32> & partially complete dir traversal

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
-use std::{io::Read, path::Path};
+use std::{fs, io::Read, num::Wrapping, path::Path};
 
 use flate2::read::ZlibDecoder;
 
 const DELTA: u32 = 0x9e3779b9;
 
 // now what was that stupid macro?
-fn mx(y: u32, z: u32, sum: u32, key: &[u32], p: u32, e: u32) -> u32 {
-    ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)) ^ ((sum ^ y) + (key[((p & 3) ^ e) as usize] ^ z))
+fn mx(
+    y: Wrapping<u32>,
+    z: Wrapping<u32>,
+    sum: Wrapping<u32>,
+    key: &[Wrapping<u32>],
+    p: Wrapping<u32>,
+    e: Wrapping<u32>,
+) -> Wrapping<u32> {
+    ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4))
+        ^ ((sum ^ y) + (key[((p & Wrapping(3)) ^ e).0 as usize] ^ z))
 }
 
 fn bdecrypt(buffer: &mut [u8], len: isize, k: u64) {
-    let key: &[u32] = &[
-        (k >> 32) as u32,
-        (k & 0xFFFFFFFF) as u32,
-        0xDEADDEAD,
-        0xB00BEEEF,
+    let key: &[Wrapping<u32>] = &[
+        Wrapping((k >> 32) as u32),
+        Wrapping((k & 0xFFFFFFFF) as u32),
+        Wrapping(0xDEADDEAD),
+        Wrapping(0xB00BEEEF),
     ];
-    let (mut y, mut z, mut sum): (u32, u32, u32);
-    let (mut p, rounds, mut e): (u32, u32, u32);
+    let (mut y, mut z, mut sum): (Wrapping<u32>, Wrapping<u32>, Wrapping<u32>);
+    let (mut p, mut rounds, mut e): (Wrapping<u32>, Wrapping<u32>, Wrapping<u32>);
 
     // CONVERT &mut [u8] TO &mut [u32]
     // let v: &mut [u32] = &mut [1, 2, 3]; //  uint32_t *v = (uint32_t *) buffer;
@@ -25,11 +33,14 @@ fn bdecrypt(buffer: &mut [u8], len: isize, k: u64) {
     // let v = buffer;
     let (prefix, v, suffix): (&mut [u8], &mut [u32], &mut [u8]);
     unsafe {
-        let (prefix, v, suffix) = buffer.align_to_mut::<u32>();
+        (prefix, v, suffix) = buffer.align_to_mut::<u32>();
     }
     if prefix.len() != 0 {
         panic!("align_to_mut is not exactly what we need then");
     }
+    if suffix.len() != 0 {
+        println!("oh cool, suffix={:?}", suffix);
+    };
 
     // VERIFY PRECONDITIONS
     let n = v.len() as u32; // let n = (len - len % 4) / 4; lua scripts just aren't u64 big anyway
@@ -37,66 +48,104 @@ fn bdecrypt(buffer: &mut [u8], len: isize, k: u64) {
         return;
     }
     // WORKING ALGORITHM
-    let mut rounds = 6 + 52 / n;
+    rounds = Wrapping(6 + 52 / n);
 
-    sum = rounds * DELTA;
-    y = v[0];
+    sum = rounds * Wrapping(DELTA);
+    // (sum, _) = rounds.overflowing_mul(DELTA);
+    y = Wrapping(v[0]);
     loop {
-        e = (sum >> 2) & 3;
-        for p in (1..n).rev() {
-            z = v[n as usize - 1];
-            v[0] -= mx(y, z, sum, key, p, e);
-            y = v[0];
+        e = (sum >> 2) & Wrapping(3);
+        for p in (1..n).rev().map(|i| Wrapping(i)) {
+            z = Wrapping(v[n as usize - 1]);
+            v[0] = v[0].wrapping_sub(mx(y, z, sum, key, p, e).0);
+            y = Wrapping(v[0]);
         }
-        z = v[n as usize - 1];
-        v[0] -= mx(y, z, sum, key, 0, e);
-        y = v[0];
+        z = Wrapping(v[n as usize - 1]);
+        v[0] = v[0].wrapping_sub(mx(y, z, sum, key, Wrapping(0), e).0);
+        y = Wrapping(v[0]);
         sum -= DELTA;
 
-        if rounds == 0 {
+        if rounds.0 == 0 {
             break;
         }
         rounds -= 1;
     }
 }
 
-fn decryptBuffer(buffer: &mut [u8]) -> bool {
+// what should this actually return?
+fn decrypt_buffer(buffer: &mut [u8]) -> Option<String> {
+    println!(
+        "buffer.len() {}, first 24bytes: {:?}",
+        buffer.len(),
+        String::from_utf8_lossy(&buffer[..24]),
+    );
     if buffer.len() < 5 {
-        return true;
+        return None; // why was true here and false in other early returns?
+                     // does it mean changed buffer or something?
     }
     if buffer[0..4] != b"ENC3"[0..4] {
         // will this comparison work?
-        return false;
+        return None;
     }
     // x86 is little endian so le, arm too
     let key: u64 = u64::from_le_bytes(buffer[4..12].try_into().unwrap());
     let compressed_size = u32::from_le_bytes(buffer[12..16].try_into().unwrap());
     let size = u32::from_le_bytes(buffer[16..20].try_into().unwrap());
     let adler = u32::from_le_bytes(buffer[20..24].try_into().unwrap());
-    if (compressed_size as usize) < buffer.len() - 24 {
-        return false;
+    // it was < but any discrepancy seems to be bad
+    println!("key: {key}, compressed_size: {compressed_size}, size: {size}, adler: {adler}\n");
+    if (compressed_size as usize) != buffer.len() - 24 {
+        return None;
     }
     bdecrypt(buffer, compressed_size as isize, key);
 
     let mut z = ZlibDecoder::new(&buffer[24..]);
     let mut s = String::new();
     z.read_to_string(&mut s).unwrap();
-    println!(s);
-    // let mut new_buffer = String::new();
-    // new_buffer.reserve(size as usize);
-    return false;
+    println!("output buffer\n{s}");
+    return Some(s);
 }
 
 // this should go through all the files in the directory, copy unencrypted and write decrypted
 // to the new outputDirectory so that we get perfect unencrypted mirror
-fn createFS(input_path: &Path) {
-    // TODO start here next time
+fn scan_dir(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
+    // CREATE EMPTY OUTPUT DIRECTORY
+    fs::create_dir(output_path)?;
+
+    // READ THE INPUT DIRECTORY
+    let res = fs::read_dir(input_path)?;
+    for f in res {
+        if let Ok(entry) = f {
+            if let Ok(ft) = entry.file_type() {
+                if ft.is_file() {
+                    println!("found file {:?}, {:?}", entry.path(), entry.metadata());
+                    if entry.file_name().to_str().unwrap().ends_with(".lua") {
+                        let mut buffer = fs::read(entry.path())?;
+                        if let Some(decrypted) = decrypt_buffer(&mut buffer) {
+                            fs::write(output_path.join(entry.file_name()), decrypted)?;
+                        }
+                        // should I skip unencrypted or just copy them?
+                    }
+                } else if ft.is_dir() {
+                    println!("found dir {:?}, {:?}", entry.path(), entry.metadata());
+                    // recreate such dir in output_path
+                } else {
+                    println!("found idk {:?}, {:?}", entry.path(), entry.metadata()?);
+                    continue;
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
 fn main() {
     let input_path = Path::new("./test");
-    createFS(input_path);
+    let output_path = Path::new("./output");
+    scan_dir(input_path, output_path);
 }
 
 #[cfg(test)]
 mod tests;
+// TODO start here next, get correct test data from c++ ver or generate it myself
