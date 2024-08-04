61f2dc7530d8c515469c21b991fc9f15b49dc1d0
pkonkol <31186361+pkonkol@users.noreply.github.com>
2024-08-05 00:18:10 +0200

    parsing the dirs recursively and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use core::panic;
 use std::{
     fs,
     io::{self, ErrorKind, Read},
     num::Wrapping,
-    path::Path,
+    path::{Path, PathBuf},
 };
 
+use anyhow::anyhow;
+use clap::{arg, command, value_parser};
 use flate2::read::ZlibDecoder;
 
 const DELTA: u32 = 0x9e3779b9;
@@ -56,45 +59,26 @@ fn bdecrypt(buffer: &mut [u8], len: isize, k: u64) {
     if n < 2 {
         return;
     }
-    // WORKING ALGORITHM
-    // TODO debug what is wrong here and why the values turn out different
+
     rounds = Wrapping(6 + 52 / n);
 
     sum = rounds * Wrapping(DELTA);
-    // (sum, _) = rounds.overflowing_mul(DELTA);
 
-    z = Wrapping(0); // tmp so i can print it
     y = Wrapping(v[0]);
-    println!(
-        "initial values: y: {}, sum: {}, rounds: {}",
-        y.0, sum.0, rounds.0
-    );
     loop {
-        println!("round {rounds} start------------------, sum: {sum},");
         e = (sum >> 2) & Wrapping(3);
         for p in (1..n).rev().map(|i| Wrapping(i)) {
             z = Wrapping(v[p.0 as usize - 1]);
-            // println!(
-            //     "y:{} z:{} sum:{} key:{:?} p:{} e:{}",
-            //     y.0, z.0, sum.0, key, p.0, e.0
-            // );
-            let pmxresult = mx(y, z, sum, key, p, e).0;
-            // print!("{}:{pmxresult}, ", p.0);
-            // panic!();
-            v[p.0 as usize] = v[p.0 as usize].wrapping_sub(pmxresult);
+            let pmx_result = mx(y, z, sum, key, p, e).0;
+            v[p.0 as usize] = v[p.0 as usize].wrapping_sub(pmx_result);
             y = Wrapping(v[p.0 as usize]);
         }
-        println!("\nafter the inner loop z: {} y: {} e: {}", z.0, y.0, e.0);
         z = Wrapping(v[n as usize - 1]);
         let mx_result = mx(y, z, sum, key, Wrapping(0), e).0;
         v[0] = v[0].wrapping_sub(mx_result);
         y = Wrapping(v[0]);
         sum -= DELTA;
 
-        println!(
-            "mx_result: {mx_result}, v[0] after sub: {}, y: {}, e: {}, z: {}",
-            v[0], y.0, e.0, z.0
-        );
         rounds -= 1;
         if rounds.0 == 0 {
             break;
@@ -102,48 +86,49 @@ fn bdecrypt(buffer: &mut [u8], len: isize, k: u64) {
     }
 }
 
-// what should this actually return?
-fn decrypt_buffer(buffer: &mut [u8]) -> Option<String> {
-    println!(
-        "buffer.len() {}, first 24bytes: {:?}",
-        buffer.len(),
-        String::from_utf8_lossy(&buffer[..24]),
-    );
+#[derive(thiserror::Error, Debug)]
+enum DecryptErr {
+    #[error("buffer is too short")]
+    TooShort,
+    #[error("not enc3 encrypted")]
+    NotENC3,
+    #[error("compress size does not match buffer len")]
+    WrongCompressedSize,
+    #[error("failed to decode zlib")]
+    ZlibDecode(#[from] io::Error),
+}
+
+fn decrypt_buffer(buffer: &mut [u8]) -> Result<String, DecryptErr> {
     if buffer.len() < 5 {
-        return None; // why was true here and false in other early returns?
-                     // does it mean changed buffer or something?
+        // not really < 24? Then I could ignore the unwraps below
+        return Err(DecryptErr::TooShort);
     }
     if buffer[0..4] != b"ENC3"[0..4] {
-        // will this comparison work?
-        return None;
+        return Err(DecryptErr::NotENC3);
     }
-    // x86 is little endian so le, arm too
+
     let key: u64 = u64::from_le_bytes(buffer[4..12].try_into().unwrap());
     let compressed_size = u32::from_le_bytes(buffer[12..16].try_into().unwrap());
+    // may this be somewhat different from full buffer and cause problesm?
     let size = u32::from_le_bytes(buffer[16..20].try_into().unwrap());
-    let adler = u32::from_le_bytes(buffer[20..24].try_into().unwrap());
-    // it was < but any discrepancy seems to be bad
-    println!("key: {key}, compressed_size: {compressed_size}, size: {size}, adler: {adler}\n");
+    // let adler = u32::from_le_bytes(buffer[20..24].try_into().unwrap());
     if (compressed_size as usize) != buffer.len() - 24 {
-        return None;
+        return Err(DecryptErr::WrongCompressedSize);
     }
-    println!("buffer with header b4 scramble=\n{:?}", buffer);
 
     bdecrypt(&mut buffer[24..], compressed_size as isize, key);
 
-    println!("buffer with header af scramble=\n{:?}", buffer);
-
     let mut z = ZlibDecoder::new(&buffer[24..]);
     let mut s = String::new();
-    z.read_to_string(&mut s).unwrap();
-    println!("output buffer\n{s}");
-    return Some(s);
+    z.read_to_string(&mut s)?;
+    // println!("output buffer\n{s}");
+    return Ok(s);
 }
 
 // this should go through all the files in the directory, copy unencrypted and write decrypted
 // to the new outputDirectory so that we get perfect unencrypted mirror
 fn scan_dir(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
-    println!("scanning dirs");
+    println!("scanning dir: {input_path:?} and outputting it to: {output_path:?}");
     // CREATE EMPTY OUTPUT DIRECTORY
     if let Err(e) = fs::create_dir(output_path) {
         match e.kind() {
@@ -157,24 +142,64 @@ fn scan_dir(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
     // READ THE INPUT DIRECTORY
     let res = fs::read_dir(input_path)?;
     for f in res {
-        println!("dir entry result: {:?}", f);
+        println!("------------- dir entry result: {:?}", f);
         if let Ok(entry) = f {
             if let Ok(ft) = entry.file_type() {
                 if ft.is_file() {
                     println!("found file {:?}, {:?}", entry.path(), entry.metadata());
                     if entry.file_name().to_str().unwrap().ends_with(".lua") {
                         let mut buffer = fs::read(entry.path())?;
-                        if let Some(decrypted) = decrypt_buffer(&mut buffer) {
-                            fs::write(output_path.join(entry.file_name()), decrypted)?;
+                        println!(
+                            "decrypting file {:?}, len: {}, buffer contents start: {:?}",
+                            entry.path(),
+                            buffer.len(),
+                            String::from_utf8_lossy(&buffer[..4]),
+                        );
+                        match decrypt_buffer(&mut buffer) {
+                            Ok(decrypted) => {
+                                fs::write(output_path.join(entry.file_name()), decrypted)?;
+                            }
+                            Err(DecryptErr::NotENC3) | Err(DecryptErr::TooShort) => {
+                                // fs.copy or something like that
+                                println!("{:?} not ENC3 encrypted, copying", entry.path());
+                                fs::copy(entry.path(), output_path.join(entry.file_name()))?;
+                            }
+                            Err(DecryptErr::ZlibDecode(inner)) => {
+                                println!(
+                                    "!!!!!{:?} got zlib decode error, inner as if that even matters: {inner}",entry.path()
+                                )
+                            }
+                            Err(DecryptErr::WrongCompressedSize) => {
+                                println!(
+                                    "!!!!!!{:?} skipping due to wrong compression size",
+                                    entry.path()
+                                );
+                            }
                         }
-                        // should I skip unencrypted or just copy them?
                     }
                 } else if ft.is_dir() {
                     println!("found dir {:?}, {:?}", entry.path(), entry.metadata());
                     // recreate such dir in output_path
+                    // let pbuf = entry.path();
+                    // let fname = pbuf
+                    //     .file_name()
+                    //     .expect("file in scanned dirs lacks filename, lol");
+                    scan_dir(
+                        &entry.path(),
+                        &output_path.join(
+                            entry.path().file_name().ok_or(anyhow::anyhow!(
+                                "file in scanned dirs lacks filename, lol"
+                            ))?,
+                        ),
+                    )?;
+                    // scan recursively
+                    // guess I could do it imperatively but i'd have to keep some queue with tuple or struct
                 } else {
-                    println!("found idk {:?}, {:?}", entry.path(), entry.metadata()?);
-                    continue;
+                    println!(
+                        "!!!!! found neither dir nor file {:?}, {:?}",
+                        entry.path(),
+                        entry.metadata()?
+                    );
                 }
             }
         }
@@ -184,9 +209,19 @@ fn scan_dir(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
 }
 
 fn main() {
-    let input_path = Path::new("./test");
-    let output_path = Path::new("./output");
-    scan_dir(input_path, output_path);
+    let matches = command!()
+        .arg(arg!([input_dir] "input directory containing ENC3 encrypted files").required(true).value_parser(value_parser!(PathBuf)))
+        .arg(arg!([output_dir] "output directory which will mirror input_dir but with decrypted files").required(true).value_parser(value_parser!(PathBuf)))
+        .get_matches();
+    let i = matches.ids();
+    println!("ids: {i:?}");
+    let input_path = matches
+        .get_one::<PathBuf>("input_dir")
+        .expect("passed wrong input path");
+    let output_path = matches
+        .get_one::<PathBuf>("output_dir")
+        .expect("passed wrong output path");
+    scan_dir(input_path, output_path).unwrap();
 }
 
 #[cfg(test)]
